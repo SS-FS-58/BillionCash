1357f236bea1af25a8857f93255fd1f0ffc8088c
SS-FS-58 <ziqiangj6@gmail.com>
2022-05-31 10:41:28 +0000

    updated version 2

--- BC_flatten_without_LP.sol
+++ BC_flatten_without_LP.sol
@@ -336,58 +336,6 @@ abstract contract Ownable is Context {
 }
 
 
-interface IDEXFactory {
-    function createPair(address tokenA, address tokenB) external returns (address pair);
-}
-
-interface IDEXRouter {
-    function factory() external pure returns (address);
-    function WETH() external pure returns (address);
-
-    function addLiquidity(
-        address tokenA,
-        address tokenB,
-        uint amountADesired,
-        uint amountBDesired,
-        uint amountAMin,
-        uint amountBMin,
-        address to,
-        uint deadline
-    ) external returns (uint amountA, uint amountB, uint liquidity);
-
-    function addLiquidityETH(
-        address token,
-        uint amountTokenDesired,
-        uint amountTokenMin,
-        uint amountETHMin,
-        address to,
-        uint deadline
-    ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
-
-    function swapExactTokensForTokensSupportingFeeOnTransferTokens(
-        uint amountIn,
-        uint amountOutMin,
-        address[] calldata path,
-        address to,
-        uint deadline
-    ) external;
-
-    function swapExactETHForTokensSupportingFeeOnTransferTokens(
-        uint amountOutMin,
-        address[] calldata path,
-        address to,
-        uint deadline
-    ) external payable;
-
-    function swapExactTokensForETHSupportingFeeOnTransferTokens(
-        uint amountIn,
-        uint amountOutMin,
-        address[] calldata path,
-        address to,
-        uint deadline
-    ) external;
-}
-
 
 // File: contracts/Billion_cash.sol
 
@@ -410,35 +358,10 @@ contract BillionCash is Ownable {
     address developmentWallet;
     address adminWallet;
     uint256 public constant PERCENTS_DIVIDER = 1000;
-    uint256 public constant MARKETING_FEE_PERCENT = 30;
-    uint256 public constant DEVELOPMENT_FEE_PERCENT = 10;
-    uint256 public constant LP_PERCENT = 100;
+    uint256 public constant MARKETING_FEE_PERCENT = 100;
+    uint256 public constant DEVELOPMENT_FEE_PERCENT = 40;
 
     mapping (address => bool) public isTransferFeeExempt; 
-
-    IDEXRouter public uniswapV2Router;
-    address pancakeV2BNBPair;
-    address[] public pairs;
-    uint256 private numTokensSellToAddToLiquidity = 10 * 10**18;
-
-    // BNB mainnet: 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c
-    // BNB testnet: 0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd
-    address WBNB = 0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd;
-
-    bool inSwapAndLiquify;
-    bool public swapAndLiquifyEnabled = false;
-
-
-    event AutoLiquify(uint256 amountBNB, uint256 amountBOG);
-    event SwapAndLiquifyEnabledUpdated(bool enabled);
-    event SwapAndLiquify(
-        uint256 tokensSwapped,
-        uint256 ethReceived,
-        uint256 tokensIntoLiqudity
-    );
-    event SwapBackFailed(string message);
-    event BuybackTransfer(bool status);
-    event SwapBackSuccess(uint256 amount);
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     
@@ -454,9 +377,9 @@ contract BillionCash is Ownable {
         // address _developmentWallet,
         // address _marketingWallet
     ) {
-        address _adminWallet = 0x2bf8429B81947D18Fc2e8A4D5E3eB3c3e5400183;
+        address _adminWallet = 0x378B826ab5639B3094cE6b0A58da097CC44b1961;
         address _developmentWallet = 0x30580573240cf9530424DbFCf8F9687f218223eC;
-        address _marketingwallet = 0x30580573240cf9530424DbFCf8F9687f218223eC;
+        address _marketingwallet = 0x13fa87bDeF0735c5794d5aCaF2e13F9589227C89;
         adminWallet = _adminWallet;
         developmentWallet = _developmentWallet;
         marketingwallet = _marketingwallet;
@@ -465,17 +388,6 @@ contract BillionCash is Ownable {
         transferOwnership(adminWallet);
         isTransferFeeExempt[adminWallet] = true;
         isTransferFeeExempt[developmentWallet] = true;
-
-        // CHANGE BEFORE DEPLOYING
-        // Router mainnet: 0x10ED43C718714eb63d5aA57B78B54704E256024E
-        // Router testnet: 0xD99D1c33F9fC3444f8101754aBC46c52416550D1
-        address dexRouter_ = 0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3;
-        uniswapV2Router = IDEXRouter(dexRouter_);
-        
-        pancakeV2BNBPair = IDEXFactory(uniswapV2Router.factory()).createPair(WBNB, address(this));
-        _allowed[address(this)][address(uniswapV2Router)] = ~uint256(0);
-        pairs.push(pancakeV2BNBPair);
-
     }
 
     function name() public pure returns (string memory) {
@@ -511,11 +423,9 @@ contract BillionCash is Ownable {
 
             uint256 BCTokenForMarketingFee = value.mul(MARKETING_FEE_PERCENT).div(PERCENTS_DIVIDER);
             uint256 BCTokenForDevelopmentFee = value.mul(DEVELOPMENT_FEE_PERCENT).div(PERCENTS_DIVIDER);
-            uint256 BCTokenForLPFee = value.mul(LP_PERCENT).div(PERCENTS_DIVIDER);
             uint256 tokensToTransfer = value
                 .sub(BCTokenForMarketingFee)
-                .sub(BCTokenForDevelopmentFee)
-                .sub(BCTokenForLPFee);
+                .sub(BCTokenForDevelopmentFee);
 
             _BCTokenBalances[to] = _BCTokenBalances[to].add(tokensToTransfer);
             _BCTokenBalances[developmentWallet] = _BCTokenBalances[developmentWallet].add(
@@ -524,27 +434,7 @@ contract BillionCash is Ownable {
             _BCTokenBalances[marketingwallet] = _BCTokenBalances[marketingwallet].add(
                 BCTokenForMarketingFee
             );
-            uint256 contractTokenBalance = balanceOf(address(this));
-            bool overMinTokenBalance = contractTokenBalance >= numTokensSellToAddToLiquidity;
-            if (
-                overMinTokenBalance &&
-                !inSwapAndLiquify &&
-                from != pancakeV2BNBPair &&
-                swapAndLiquifyEnabled
-            ){
-                emit SwapAndLiquify(0, BCTokenForLPFee, _BCTokenBalances[address(this)]);
-                contractTokenBalance = numTokensSellToAddToLiquidity;
-                swapAndLiquify(contractTokenBalance);
-                emit SwapAndLiquify(0, BCTokenForLPFee, _BCTokenBalances[address(this)]);
-                _BCTokenBalances[address(this)] = _BCTokenBalances[address(this)].add(BCTokenForLPFee);
-                emit Transfer(from, pancakeV2BNBPair, BCTokenForLPFee);  
-            }
-            else{
-                _BCTokenBalances[address(this)] = _BCTokenBalances[address(this)].add(
-                    BCTokenForLPFee
-                );
-                emit Transfer(from, address(this), BCTokenForLPFee);  
-            }
+
             emit Transfer(msg.sender, developmentWallet, BCTokenForDevelopmentFee);
             emit Transfer(msg.sender, marketingwallet, BCTokenForMarketingFee);
         }
@@ -583,112 +473,6 @@ contract BillionCash is Ownable {
         // _allowed[from][msg.sender] = _allowed[from][msg.sender].sub(value);
         emit Transfer(from, to, value);
         return true;
-    }
-    function addLiquidity(uint256 tokenAmount, uint256 ethAmount) private {
-        // approve token transfer to cover all possible scenarios
-        _approve(address(this), address(uniswapV2Router), tokenAmount);
-
-        // add the liquidity
-        try uniswapV2Router.addLiquidityETH{value: ethAmount}(
-            address(this),
-            tokenAmount,
-            0, // slippage is unavoidable
-            0, // slippage is unavoidable
-            owner(),
-            block.timestamp
-        ){
-            emit AutoLiquify(tokenAmount, ethAmount);
-        } catch {
-            emit AutoLiquify(0, 0);
-        }
-    }
-
-    modifier lockTheSwap {
-        inSwapAndLiquify = true;
-        _;
-        inSwapAndLiquify = false;
-    }
-
-    function swapAndLiquify(uint256 contractTokenBalance) private lockTheSwap {
-        // split the contract balance into halves
-        emit SwapAndLiquify(0, 0, contractTokenBalance);
-        uint256 half = contractTokenBalance.div(2);
-        uint256 otherHalf = contractTokenBalance.sub(half);
-
-        // capture the contract's current ETH balance.
-        // this is so that we can capture exactly the amount of ETH that the
-        // swap creates, and not make the liquidity event include any ETH that
-        // has been manually sent to the contract
-        uint256 initialBalance = address(this).balance;
-
-        // swap tokens for ETH
-        // swapTokensForEth(half); // <- this breaks the ETH -> HATE swap when swap+liquify is triggered
-        address[] memory path = new address[](2);
-        path[0] = address(this);
-        // path[1] = uniswapV2Router.WETH();
-        path[1] = WBNB;
-        try uniswapV2Router.swapExactTokensForETHSupportingFeeOnTransferTokens(
-            half,
-            0,
-            path,
-            address(this),
-            block.timestamp
-        ) {
-
-            uint256 amountBNB = address(this).balance.sub(initialBalance);
-            if(amountBNB > 0){
-                try uniswapV2Router.addLiquidityETH{ value: amountBNB }(
-                    address(this),
-                    otherHalf,
-                    0,
-                    0,
-                    address(this),
-                    block.timestamp
-                ) {
-                    emit AutoLiquify(otherHalf, amountBNB);
-                } catch {
-                    emit AutoLiquify(0, 0);
-                }
-            }
-
-            emit SwapBackSuccess(amountBNB);
-        } catch Error(string memory e) {
-            emit SwapBackFailed(string(abi.encodePacked("SwapBack failed with error ", e)));
-        } catch {
-            emit SwapBackFailed("SwapBack failed without an error message from pancakeSwap");
-        }
-        // how much ETH did we just swap into?
-        uint256 newBalance = address(this).balance.sub(initialBalance);
-        emit SwapAndLiquify(half, newBalance, otherHalf);
-
-        // add liquidity to uniswap
-        addLiquidity(otherHalf, newBalance);
-        
-        emit SwapAndLiquify(half, newBalance, otherHalf);
-    }
-    function swapTokensForEth(uint256 tokenAmount) private {
-        // generate the uniswap pair path of token -> weth
-        address[] memory path = new address[](2);
-        path[0] = address(this);
-        path[1] = uniswapV2Router.WETH();
-        // path[1] = WBNB;
-
-        _approve(address(this), address(uniswapV2Router), tokenAmount);
-
-        emit SwapAndLiquify(tokenAmount, tokenAmount, 1);
-        // make the swap
-        try uniswapV2Router.swapExactTokensForETHSupportingFeeOnTransferTokens(
-            tokenAmount,
-            0, // accept any amount of ETH
-            path,
-            address(this),
-            block.timestamp
-        ){
-        } catch Error(string memory e) {
-            emit SwapBackFailed(string(abi.encodePacked("SwapBack failed with error ", e)));
-        } catch {
-            emit SwapBackFailed("SwapBack failed without an error message from pancakeSwap");
-        }
     }
 
     function transferFrom(
@@ -739,9 +523,5 @@ contract BillionCash is Ownable {
     }
     function setIsTransferFeeExempt(address holder, bool exempt) external onlyOwner{
         isTransferFeeExempt[holder] = exempt;
-    }
-    function setSwapAndLiquifyEnabled(bool _enabled) public onlyOwner {
-        swapAndLiquifyEnabled = _enabled;
-        emit SwapAndLiquifyEnabledUpdated(_enabled);
     }
 }
